cd15c6839503f60faec716be5215f29762d865b2
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-02-27 21:21:21 -0500

    making a mess on the landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -10,6 +10,20 @@ const Title = styled.h1`
 const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
+  display: block;
+`;
+
+const CardWrapper = styled.div`
+  width: 10rem;
+  height: 10rem;
+  border: 1px solid red;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  margin: auto;
+  margin-bottom: 0.5rem;
+  border: 1px solid red;
 `;
 
 // Use Title and Wrapper like any other React component – except they're styled!
@@ -17,6 +31,9 @@ export function LandingPage() {
   return (
     <Wrapper>
       <Title>Hello World!</Title>
+      <CardWrapper>
+        <Input></Input>
+      </CardWrapper>
     </Wrapper>
   );
 }
