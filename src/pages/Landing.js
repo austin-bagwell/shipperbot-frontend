c5bc53c0bf0bb3a4878a4320ef6ccf5628402a92
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-02-27 20:06:21 -0500

    rm CRA boilerplate, add styled-components

---
+++ src/pages/Landing.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+// Use Title and Wrapper like any other React component – except they're styled!
+export function LandingPage() {
+  return (
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
+  );
+}
