4879476d102a9ab20cb7e794534ed27063bef7fc
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-03-12 16:07:51 -0400

    remove unused imports from index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import styled from "styled-components";
 import App from "./App";
-import { LoginPage } from "./pages/LoginPage.js";
-import { Navbar } from "./components/Navbar.js";
 import "./global.css";
 
-const Heading = styled.h1`
-  color: purple;
-  text-align: center;
-`;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const submit = (e) => {
-  e.preventDefault();
-  console.log(`clicked submit button`);
-};
 
 root.render(
   <React.StrictMode>
