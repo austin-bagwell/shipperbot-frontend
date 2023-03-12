07b6364ac595940126fa3d86184c63f9429890b1
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-03-12 16:17:18 -0400

    added silly copy to landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,31 +9,27 @@ const Title = styled.h1`
 // Create a Wrapper component that'll render a <section> tag with some styles
 const Wrapper = styled.section`
   padding: 4em;
-  background: papayawhip;
+  background: lightgrey;
   display: block;
-`;
-
-const CardWrapper = styled.div`
-  width: 10rem;
-  height: 10rem;
-  border: 1px solid red;
-  margin: auto;
-`;
-
-const Input = styled.input`
-  margin: auto;
-  margin-bottom: 0.5rem;
-  border: 1px solid red;
+  text-align: center;
 `;
 
 // Use Title and Wrapper like any other React component – except they're styled!
 export function LandingPage() {
   return (
-    <Wrapper>
-      <Title>Hello World!</Title>
-      <CardWrapper>
-        <Input></Input>
-      </CardWrapper>
-    </Wrapper>
+    <main>
+      <Title>Welcome to shipperBot!</Title>
+      <Wrapper>
+        <h2>What is shipperBot?</h2>
+        <p>
+          shipperBot is designed to help schedule shipments for on-time arrival.
+        </p>
+        <p>
+          Feed shipperBot a due date, and its advanced AI-powered algorithm*
+          will tell you when the order should ship.
+        </p>
+      </Wrapper>
+      <h6 style={{ textAlign: "center" }}>*okay not AI, just basic math</h6>
+    </main>
   );
 }
