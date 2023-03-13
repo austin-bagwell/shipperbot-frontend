f0c9b9613017b747292606f1c8847bd1de8caed4
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-03-12 21:07:31 -0400

    make logo redirect to room

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,21 @@ import { Link } from "react-router-dom";
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
-  padding: 1rem 2rem 1rem; 2rem;
+  align-items: center;
+  padding: 0.3rem 2rem 0.5rem; 2rem;
   border-bottom: 2px solid black;
 `;
 
 const LinkWrapper = styled.ul`
   display: inline-block;
+
+  a {
+    color: black;
+  }
+
+  a:hover {
+    color: darkorange;
+  }
 `;
 
 const Crumb = styled.li`
@@ -18,24 +27,29 @@ const Crumb = styled.li`
   padding-left: 1rem;
 `;
 
-const Button = styled.button`
-  background: blue;
-  color: white;
-  border-radius: 3px;
-`;
-
 const StyledLink = styled(Link)`
   text-decoration: none;
   font-size: 1.5rem;
   font-weight: 700;
-  letter-spacing: 0.04em;
   margin: 1rem;
 `;
 
-function Navbar(props) {
+const Logo = styled.h1`
+  font-size: 3.5em;
+  letter-spacing: 0.07em;
+  margin: 0;
+`;
+
+// TODO is linkToRoot a good way to make my logo/title bit redirect to / ?
+function Navbar() {
+  const linkToRoot = (e) => {
+    e.preventDefault();
+    window.location.href = "/";
+  };
+
   return (
     <Nav>
-      <Button>Mock Logo</Button>
+      <Logo onClick={linkToRoot}>shipperBot</Logo>
       <LinkWrapper>
         <Crumb>
           <StyledLink to="/login">Login</StyledLink>
