cbf79ea43df12ac5355c261c922d111ea303af48
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-03-16 11:01:57 -0400

    add basic consignee page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
-import Landing from "./pages/Landing.js";
-import Login from "./pages/Login.js";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Consignees from "./pages/Consignees";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Landing />}></Route>
           <Route path="login" element={<Login />}></Route>
           <Route path="signup" element={<Signup />}></Route>
+          <Route path="consignees" element={<Consignees />}></Route>
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Consignees.js
@@ -0,0 +1,10 @@
+// import styled from "styled-components";
+
+export default function Consignees(props) {
+  return (
+    <div>
+      <p>hello consignees</p>
+      <p>hello world?</p>
+    </div>
+  );
+}
