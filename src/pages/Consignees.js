96af0f2fb37927348566e83e163bd39fe6b19f6a
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-03-16 11:56:42 -0400

    truly atrocious consignee table attempt

--- src/pages/Consignees.js
+++ src/pages/Consignees.js
@@ -1,10 +1,37 @@
 import styled from "styled-components";
 import { MainContentWrapper } from "../components/wrappers/flexWrappers";
 
+const SearchWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  border: 1px solid red;
+`;
+
+const Table = styled.table`
+  border: 1px solid black;
+  margin: auto;
+`;
+
 export default function Consignees(props) {
   return (
     <MainContentWrapper>
-      <p>hello consignees</p>
+      <SearchWrapper>
+        <input type={"text"}></input>
+        <button>Batch Upload</button>
+        <button>Add New</button>
+      </SearchWrapper>
+      <Table>
+        <tr>
+          <th>Name</th>
+          <th>Transit Time</th>
+          <th>Actions</th>
+        </tr>
+        <tr>
+          <td>Target</td>
+          <td>666</td>
+          <td>action stuff</td>
+        </tr>
+      </Table>
       {/* search bar + batch upload btn + add new btn */}
       {/* tabular consignee data */}
       {/* pagination options */}
