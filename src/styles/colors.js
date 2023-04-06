732f6b48f35bbc3b9c0276e1ecd7267b0a870225
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-04-05 21:00:42 -0400

    add styles dir and colors.js

---
+++ src/styles/colors.js
@@ -0,0 +1,28 @@
+/* BASE COLORS */
+export const colorWhite = "#ffffff";
+export const colorBlack = "#262626";
+
+export const colorGrey = "#82908d"; // green/grey
+
+export const colorGreen = "#89af5b";
+export const colorRed = "#b04846";
+export const colorLightBlue = "#ADD8E6";
+export const colorYellow = "#d1d156";
+
+/* COLORS */
+export const colorPrimary = "#3d8f9d";
+export const colorBackground = colorWhite;
+
+export const colorFontPrimary = colorBlack;
+
+export const colorSuccess = colorLightBlue;
+export const colorCaution = colorRed;
+export const colorWarning = colorYellow;
+
+/**
+ * greyScale
+ * @param {number} degree - Number, 0 - 10. 0 being black 10 being white
+ */
+export function greyScale(degree) {
+  return `hsl(0, 0%, ${degree * 10}%)`;
+}
