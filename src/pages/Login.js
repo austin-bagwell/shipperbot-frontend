18d0f49600e1441bdd7dd7873b245f1e35d5d677
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-04-08 14:13:29 -0400

    add background image to MainContentWrapper element - works only on Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,33 @@
 import { MainContentWrapper } from "../components/wrappers/flexWrappers";
 import { AuthForm } from "../components/AuthForm";
 import { LabeledInput } from "../components/elements/Input/LabeledInput";
-import BackgroundImage from "../components/BackgroundImg";
+import Background from "../assets/background_1.jpg";
+
 export default function Login() {
   return (
-    <BackgroundImage>
-      <MainContentWrapper>
-        <AuthForm>
-          <div>
-            <LabeledInput
-              type="text"
-              name="username"
-              label="username: "
-            ></LabeledInput>
-            <LabeledInput
-              type="password"
-              name="password"
-              label="password: "
-            ></LabeledInput>
-          </div>
-        </AuthForm>
-      </MainContentWrapper>
-    </BackgroundImage>
+    <MainContentWrapper
+      style={{
+        backgroundImage: `url(${Background}`,
+        backgroundPosition: "center",
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <AuthForm>
+        <div>
+          <LabeledInput
+            type="text"
+            name="username"
+            label="username: "
+          ></LabeledInput>
+          <LabeledInput
+            type="password"
+            name="password"
+            label="password: "
+          ></LabeledInput>
+        </div>
+      </AuthForm>
+    </MainContentWrapper>
   );
 }
