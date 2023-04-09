12157276fb59ce285ac8bb84c7fe3b00a298e4f1
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-04-09 19:16:50 -0400

    add the most basic of all tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders landing page header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/What is shipperBot\?/i);
   expect(linkElement).toBeInTheDocument();
 });
